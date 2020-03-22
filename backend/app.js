268e27c82521b3634921c9344c240952c8ef5413
Mathias Davidsen <matze.davidsen@icloud.com>
2020-03-22 18:12:54 +0800

    add backend

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const express = require('express')
+
+const router = express.Router();
+
+mongoose.Promise = global.Promise;
+
+//db connect
+mongoose.connect('mongodb://77.68.91.80/divegigs', {	
+													useNewUrlParser: true, 
+													useUnifiedTopology: true, 
+													auth: { "authSource": "divegigs" }, 
+													user: "matt", 
+													pass: "@dg2020"
+												});
